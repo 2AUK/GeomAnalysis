3f0ec11239bcd7dddc45424ea0553591541f39bf
abdullah <abdullah@abdullah.home>
2018-08-01 21:20:28 +0100

    Parts of it are working now

--- main.c
+++ main.c
@@ -10,9 +10,16 @@ int main(){
     molxyz =  fopen("acetaldehyde.txt", "r");
     molecule_read(molxyz, &mol);
 
-
     printf("%d\n", mol.natom);
-    
+    for (int i = 0; i < mol.natom; i++)
+        printf("%d\t", mol.zvals[i]);
+    putchar('\n');
+    for (int j = 0; j < 3*mol.natom; j+=3){
+        for (int k = 0; k < 3; k++){
+            printf("%3.6lf\t", mol.geom[j+k]);
+        }
+        putchar('\n');
+    }
     return 0;
 }
 
